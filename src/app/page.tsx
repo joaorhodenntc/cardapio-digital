dc4cffb4e85e3c876d52820e90a5d781d6e33cb8
joaorhodenntc <jvrhodenmachado@hotmail.com>
2025-02-14 15:28:38 -0300

    Alterei pagina inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,118 @@
-// src/pages/index.js
-import React from "react";
-import Link from "next/link";
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+import MenuSection from "./_components/MenuSection";
+import { menuData } from "../data/menuData";
+import Image from "next/image";
+import Options from "./_components/Option";
 
-const Home = () => {
+const Cardapio = () => {
+  const [imageSrc, setImageSrc] = useState("/brita-logo.png");
+  const [isFixed, setIsFixed] = useState(false);
+  const [selectedSection, setSelectedSection] = useState("chopes");
+
+  // Referências para as seções
+  const chopesRef = useRef(null);
+  const cevasRef = useRef(null);
+  const drinksAutoraisRef = useRef(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setImageSrc("/brita-logo-lg.png");
+      } else {
+        setImageSrc("/brita-logo.png");
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  // Efeito para observar as seções e atualizar o select
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setSelectedSection(entry.target.id);
+          }
+        });
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5, // 50% da seção precisa estar visível
+      }
+    );
+
+    // Observa as seções
+    if (chopesRef.current) observer.observe(chopesRef.current);
+    if (cevasRef.current) observer.observe(cevasRef.current);
+    if (drinksAutoraisRef.current) observer.observe(drinksAutoraisRef.current);
+
+    // Limpa o observer ao desmontar o componente
+    return () => {
+      if (chopesRef.current) observer.unobserve(chopesRef.current);
+      if (cevasRef.current) observer.unobserve(cevasRef.current);
+      if (drinksAutoraisRef.current) {
+        observer.unobserve(drinksAutoraisRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">
-        Bem-vindo ao Restaurante
-      </h1>
-      <p className="text-lg text-gray-600 mb-8">
-        Confira nosso cardápio digital e faça seu pedido!
-      </p>
-      <Link href={"/cardapio"}>
-        <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition duration-300">
-          Ver Cardápio
-        </button>
-      </Link>
+    <div className="mt-5">
+      <Image
+        src={imageSrc}
+        alt="Logo"
+        layout="responsive"
+        width={100}
+        height={100}
+      />
+
+      <Options
+        items={menuData}
+        isFixed={isFixed}
+        categorySelected={selectedSection}
+        setCategorySelected={setSelectedSection}
+      />
+
+      <div className="container mx-auto px-4 pt-8 mt-20 relative">
+        <div id="chopes" ref={chopesRef} className="scroll-mt-20 relative">
+          <MenuSection title="CHOPES" items={menuData.chopes} />
+        </div>
+        <div id="cevas" ref={cevasRef} className="scroll-mt-20">
+          <MenuSection title="CEVAS" items={menuData.cevas} />
+        </div>
+        <div
+          id="drinksAutorais"
+          ref={drinksAutoraisRef}
+          className="scroll-mt-20"
+        >
+          <MenuSection
+            title="DRINKS AUTORAIS"
+            items={menuData.drinksAutorais}
+          />
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default Cardapio;
